f551da79f7418e6056ea50d29d0f07bcd1a6a390
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2021-05-26 13:07:07 +0200

    bring the products compoonents 23:23 video

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 import Header from './components/Header'
 import Main from './components/Main';
 import Basket from './components/Basket';
+import data from './data';
+
 
 function App() {
+  const {products} = data;
   return (
     <div className="App">
       <Header></Header>
-      <div>
-      <Main>
-      </Main>
-      <Basket>
-      </Basket> 
+      <div className="row">
+        <Main products={products}>
+        </Main>
+        <Basket>
+        </Basket>
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Macbook',
+            price: 1400,
+            image: 'https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=757&q=80',
+        },
+        {
+            id: '2',
+            name: 'Lenovo',
+            price: 1200,
+            image: 'https://images.unsplash.com/photo-1497171156029-51dfc973e5f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+        },
+        {
+            id: '3',
+            name: 'Surface',
+            price: 900,
+            image: 'https://images.unsplash.com/photo-1553117535-5edfe8ab4b8a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80',
+        },
+    ],
+};
+export default data;
