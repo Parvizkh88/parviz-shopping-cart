7a9e7061b6c8c2eb9c3212d41f0e690950ff1889
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2021-05-27 08:15:48 +0200

    do the down part of basket  to have a summary of shopping cart minute 42:30

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function Basket(props) {
     const { cartItems, onAdd, onRemove } = props;
+    const itemPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
+    const taxPrice = itemPrice * 0.14;
+    const shippingPrice = itemPrice > 2000 ? 0 : 50;
+    const totalPrice = itemPrice + taxPrice + shippingPrice;
+
     return (<aside className="block col-1">
         <h2>Cart Items</h2>
         <div>
@@ -14,10 +19,31 @@ export default function Basket(props) {
                         <button onClick={() => onRemove(item)} className="remove">-</button>
                     </div>
                     <div className="col-2 text-right">
-                    {item.qty}*{item.price.toFixed(2)}
+                        {item.qty}*{item.price.toFixed(2)}
                     </div>
                 </div>
             ))}
         </div>
+        {cartItems.length !== 0 && (
+            <>
+                <hr></hr>
+                <div className="row">
+                    <div className="cal-2">Item Price</div>
+                    <div className="cal-1 text-right">${itemPrice.toFixed(2)}</div>
+                </div>
+                <div className="row">
+                    <div className="cal-2">Tax Price</div>
+                    <div className="cal-1 text-right">${taxPrice.toFixed(2)}</div>
+                </div>
+                <div className="row">
+                    <div className="cal-2">Shipping Price</div>
+                    <div className="cal-1 text-right">${shippingPrice.toFixed(2)}</div>
+                </div>
+                <div className="row">
+                    <div className="cal-2"><strong>Total Price</strong></div>
+                    <div className="cal-1 text-right"><strong>${totalPrice.toFixed(2)}</strong></div>
+                </div>
+            </>
+        )}
     </aside>);
 }
